5bc0c712f947ce4986d103a1a48d667bbe4875ae
wei2912 <wei2912_support@hotmail.com>
2012-09-23 14:55:15 +0800

    Added non-working fstab reader.

---
+++ fstabReader.py
@@ -0,0 +1,9 @@
+# Meant to parse fstab
+# Seperate content into different columns
+# For now it's the whole file.
+
+# DOES NOT WORK BECAUSE WEI2912 IS A PYTHON NOOB
+file = open(r'/etc/fstab', 'r').readlines()
+for line in logfile:
+	print line
+
