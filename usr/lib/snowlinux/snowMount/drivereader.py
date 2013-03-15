07156044fb61c9aef352cef0a2b172c1c48170e4
Andy Jacobsen <atze@libra.uberspace.de>
2013-03-15 21:10:00 +0100

    improved drivereader

--- usr/lib/snowlinux/snowMount/drivereader.py
+++ usr/lib/snowlinux/snowMount/drivereader.py
@@ -1,6 +1,42 @@
 # -*- coding: utf-8 -*-
 
 import parted
+import subprocess
+
+class DriveReader(object):
+    def __init__(self):
+        self.disks = Disks()._getDisks()
+
+    def getModel(self, device_path):
+        if device_path[-1].isdigit():
+            return self.disks[device_path[:-1]]['model']
+        else:
+            return self.disks[device_path]['model']
+
+    def getSize(self, device_path):
+        if device_path[-1].isdigit():
+            return self.disks[device_path[:-1]]['parts'][device_path]['size']
+        else:
+            return self.disks[device_path]['size']
+
+    def getFilesystem(self, device_path):
+        return self.disks[device_path[:-1]]['parts'][device_path]['filesystem']
+
+    def getUUID(self, device_path):
+        return self.disks[device_path[:-1]]['parts'][device_path]['uuid']
+
+    def getLabel(self, device_path):
+        return self.disks[device_path[:-1]]['parts'][device_path]['label']
+
+    def getDevice(self, label=None, uuid=None):
+        if label:
+            dev = subprocess.check_output('blkid -L {}'.format(label).strip(), shell=True)
+            return dev.strip()
+        elif uuid:
+            dev = subprocess.check_output('blkid -U {}'.format(uuid).strip(), shell=True)
+            return dev.strip()
+        else:
+            return None
 
 class Disks(object):
     '''
@@ -19,7 +55,7 @@ class Disks(object):
                                             'size': disk.size,
                                             'parts': disk.partitions}
 
-    def getDisks(self):
+    def _getDisks(self):
         return self._disks
 
 class Disk(object):
@@ -32,7 +68,9 @@ class Disk(object):
         for part in self._disk.partitions:
             part = Partition(part)
             self.partitions[part.device_path] = {'size': part.size,
-                                                'filesystem': part.filesystem}
+                                                'filesystem': part.filesystem,
+                                                'uuid': part.uuid,
+                                                'label': part.label}
 
 class Partition(object):
     def __init__(self, partition):
@@ -41,4 +79,17 @@ class Partition(object):
         self.device_name = self._partition.getDeviceNodeName()
         self.device_path = self._partition.path
         self.filesystem = self._partition.fileSystem.type
-        
+        p = subprocess.check_output(['lsblk', '-Po', 'UUID', self.device_path])
+        self.uuid = p.split('=')[1].strip().strip('"')
+        p = subprocess.check_output(['lsblk', '-Po', 'LABEL', self.device_path])
+        self.label = p.split('=')[1].strip().strip('"')
+
+
+if __name__ == '__main__':
+    dr = DriveReader()
+    for disk in dr.disks:
+        print 'Disk: {}, Model: {} Size: {}'.format(disk, dr.getModel(disk), dr.getSize(disk))
+        # print 'Device\tLabel\tSize\tFilesystem\tUUID'
+        for part in dr.disks[disk]['parts']:
+            print '-{}\t{}\t{}\t{}\t{}'.format(part, dr.getLabel(part), dr.getSize(part), dr.getFilesystem(part), dr.getUUID(part).strip('"'))
+
